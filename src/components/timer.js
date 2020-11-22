c6f8809a2efe61c5a24f9aa38644c9c5e4d324fb
siddiq <msm2029@gmail.com>
2020-11-22 22:21:01 +0400

    bootstrap changes to the website

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./timer.css";
+import topLeft from "../img/khalifa-uni1.png";
+import topRight from "../img/logos.png";
+import welcome from "../img/welcome1.png";
+import sheikh from "../img/sheikh.png";
 
 function App() {
   const calculateTimeLeft = () => {
@@ -12,19 +16,18 @@ function App() {
     if (difference > 0) {
       var dd = Math.floor(difference / (1000 * 60 * 60 * 24));
       var hh = Math.floor((difference / (1000 * 60 * 60)) % 24);
-      var mm =  Math.floor((difference / 1000 / 60) % 60);
+      var mm = Math.floor((difference / 1000 / 60) % 60);
       var ss = Math.floor((difference / 1000) % 60);
-      if(dd > 0){
+      if (dd > 0) {
         hh = hh + 24;
+      } else if (hh < 10) {
+        hh = "0" + hh;
       }
-      else if (hh < 10){
-        hh = "0" + hh
+      if (mm < 10) {
+        mm = "0" + mm;
       }
-      if (mm < 10){
-        mm = "0" + mm
-      }
-      if (ss < 10){
-        ss = "0" + ss
+      if (ss < 10) {
+        ss = "0" + ss;
       }
       timeLeft = {
         // days: Math.floor(difference / (1000 * 60 * 60 * 24)),
@@ -56,22 +59,70 @@ function App() {
     timerComponents.push(<span>{timeLeft[interval]} </span>);
   });
   return (
-    <div className="timer">
-      {/* <h1>Khalifa University {year} Countdown</h1> */}
-      {/* <h2>With React Hooks!</h2> */}
-      <div className="timerMain">
-        <h2 >
-          {timerComponents.length ? timerComponents : <span>Time's up!</span>}
-          {/* <span>{timeLeft}</span> */}
-        </h2>
+    <>
+      <div className="section-1">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <img class="img-fluid float-left tLeft" src={topLeft}></img>
+            </div>
+            <div class="col">
+              <img class="img-fluid float-right" src={topRight}></img>
+            </div>
+          </div>
+        </div>
       </div>
-      <h2>
-        {/* <span style={{ position: "absolute", top: "730px", left: "150px" }}>Days</span> */}
-        {/* <span style={{ position: "absolute", top: "730px", left: "150px" }}>Hours</span>
-        <span style={{ position: "absolute", top: "730px", left: "241px" }}>Minutes</span>
-        <span style={{ position: "absolute", top: "730px", left: "350px" }}>Seconds</span> */}
-      </h2>
-    </div>
+
+      <div class="container mt-3">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="row">
+              <div class="col-12">
+                <div class="sidebar-content">
+                  <img class="img-fluid float-left" src={welcome}></img>
+                  <h3>Tuesday, 24 November 2020</h3>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="sidebar-content"></div>
+              </div>
+              <div class="col-12">
+                <div class="sidebar-content">
+                  <div className="timer">
+                    {/* <div className="timerMain"> */}
+                    <h2>
+                      <div className="timerText ">
+                        <div class="row mx-auto justify-content-center align-items-center flex-row">
+                          <div className="col-sm-4">{timerComponents[0]}</div>
+                          <div className="col-sm-4">{timerComponents[1]}</div>
+                          <div className="col-sm-4">{timerComponents[2]}</div>
+                        </div>
+                        <div class="row">
+                          <div className="col-sm-4">Hours</div>
+                          <div className="col-sm-4">Minutes</div>
+                          <div className="col-sm-4">Seconds</div>
+                        </div>
+                      </div>
+                    </h2>
+                    {/* </div> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-8">
+            <div class="main-content">
+              <img class="img-fluid float-right" src={sheikh} style={{width: "60%"}}></img>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br></br>
+{/* 
+      <div className="timeSection">
+        <h3>Tuesday, 24 November 2020</h3>
+      </div> */}
+    </>
   );
 }
 
