a405a8fadb60418f185e84b94c1ee4e176f5bdb0
siddiq <msm2029@gmail.com>
2020-11-23 16:00:09 +0400

    added 10s buffer and changed to sharper images

--- src/components/timer.js
+++ src/components/timer.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./timer.css";
 import topLeft from "../img/khalifa-uni1.png";
 import topRight from "../img/logos.png";
-import welcome from "../img/welcome2.png";
+import welcome from "../img/welcome.png";
 import sheikh from "../img/sheikh1.png";
 
 // import { fainstagram } from "@fortawesome/free-solid-svg-icons";
@@ -14,7 +14,11 @@ import {
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 
+var flag = 0;
+
 function App() {
+  const [isTimeOver, setIsTimeOver] = useState(false);
+
   const calculateTimeLeft = () => {
     // var currentLocation = window.location;
     // console.log(currentLocation);
@@ -53,8 +57,13 @@ function App() {
         seconds: ss,
       };
     }
-    if (difference < 1) {
-      window.location.href = "https://holofair.app/thh/?event=zone&THH";
+    if (difference < 1 && flag === 0) {
+    // if (ss < 3 && flag === 0) {
+      setIsTimeOver(true);
+      setTimeout(() => {
+        window.location.href = "https://holofair.app/thh/?event=zone&THH";
+      }, 10000);
+      flag = 1;
     }
     // console.log("timeLeft: ", timeLeft.seconds);
     return timeLeft;
@@ -80,92 +89,99 @@ function App() {
   });
   return (
     <>
-      <div className="section-1">
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <img
-                alt="Khalifa University"
-                className="img-fluid float-left tLeft"
-                src={topLeft}
-              ></img>
-            </div>
-            <div className="col">
-              <img
-                alt="Seeds Of The Union"
-                className="img-fluid float-right tright"
-                src={topRight}
-              ></img>
+      <div
+        className="instruction-page"
+        style={{ visibility: isTimeOver ? "hidden" : "visible" }}
+      >
+        <div className="section-1">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <img
+                  alt="Khalifa University"
+                  className="img-fluid float-left tLeft"
+                  src={topLeft}
+                ></img>
+              </div>
+              <div className="col">
+                <img
+                  alt="Seeds Of The Union"
+                  className="img-fluid float-right tright"
+                  src={topRight}
+                ></img>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="container mt-3 mainContainer">
-        <div className="row">
-          <div className="col-sm-4">
-            <div className="row">
-              <div className="col-12">
-                <div className="sidebar-content">
-                  <img
-                    alt="Welcome to KU Virtual National Day Ceremony 2020"
-                    className="img-fluid float-left"
-                    src={welcome}
-                  ></img>
-                  <br />
-                  <h3> Tuesday, 24 November 2020</h3>
-                  <br />
+        <div className="container mt-3 mainContainer">
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="row">
+                <div className="col-12">
+                  <div className="sidebar-content">
+                    <img
+                      alt="Welcome to KU Virtual National Day Ceremony 2020"
+                      className="img-fluid float-left"
+                      src={welcome}
+                    ></img>
+                    <br />
+                    <h3> Tuesday, 24 November 2020</h3>
+                    <br />
+                  </div>
                 </div>
-              </div>
-              <div className="col-12">
-                <div className="sidebar-content">
-                  <div className="timer">
-                    <h2>
-                      <div className="timerText">
-                        <div className="row text-center numbers">
-                          <div className="col-4 col-xs-4">
-                            {timerComponents[0]}
+                <div className="col-12">
+                  <div className="sidebar-content">
+                    <div className="timer">
+                      <h2>
+                        <div className="timerText">
+                          <div className="row text-center numbers">
+                            <div className="col-4 col-xs-4">
+                              {timerComponents[0]}
+                            </div>
+                            <div className="col-4 col-xs-4">
+                              {timerComponents[1]}
+                            </div>
+                            <div className="col-4 col-xs-4">
+                              {timerComponents[2]}
+                            </div>
                           </div>
-                          <div className="col-4 col-xs-4">
-                            {timerComponents[1]}
-                          </div>
-                          <div className="col-4 col-xs-4">
-                            {timerComponents[2]}
+                          <div className="row text-center texts">
+                            <div className="col-4 col-xs-4">
+                              <span>Hours</span>
+                            </div>
+                            <div className="col-4 col-xs-4">
+                              <span>Minutes</span>
+                            </div>
+                            <div className="col-4 col-xs-4">
+                              <span>Seconds</span>
+                            </div>
                           </div>
                         </div>
-                        <div className="row text-center texts">
-                          <div className="col-4 col-xs-4">
-                            <span>Hours</span>
-                          </div>
-                          <div className="col-4 col-xs-4">
-                            <span>Minutes</span>
-                          </div>
-                          <div className="col-4 col-xs-4">
-                            <span>Seconds</span>
-                          </div>
-                        </div>
-                      </div>
-                    </h2>
+                      </h2>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-8">
-            <div className="main-content">
-              <img
-                alt="Sheikh"
-                className="img-fluid float-right sheikh"
-                src={sheikh}
-              ></img>
+            <div className="col-sm-8">
+              <div className="main-content">
+                <img
+                  alt="Sheikh"
+                  className="img-fluid float-right sheikh"
+                  src={sheikh}
+                ></img>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="timeSection">
-        <h3>
-          #KU2020NationalDay {" "} {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
-          {/* <div className="social-icons"> */}
+        <div className="timeSection">
+          <h3>
+            #KU2020NationalDay {"\u00A0"}
+            {"\u00A0"}
+            {"\u00A0"}
+            {"\u00A0"}
+            {/* <div className="social-icons"> */}
             <a href="https://www.instagram.com/khalifa_university/">
               <FontAwesomeIcon icon={faInstagram} style={{ color: "black" }} />{" "}
             </a>
@@ -178,8 +194,9 @@ function App() {
             <a href="https://twitter.com/KhalifaUni">
               <FontAwesomeIcon icon={faTwitter} style={{ color: "black" }} />{" "}
             </a>
-          {/* </div> */}
-        </h3>
+            {/* </div> */}
+          </h3>
+        </div>
       </div>
     </>
   );
