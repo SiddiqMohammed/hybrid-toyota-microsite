fdf00e96c1fa486a5246cff32cb577b69783a3d3
siddiq <msm2029@gmail.com>
2020-11-29 13:33:56 +0400

    added validation and form after that

--- src/components/timer.js
+++ src/components/timer.js
@@ -4,6 +4,7 @@ import topLeft from "../img/khalifa-uni1.png";
 import topRight from "../img/logos.png";
 import welcome from "../img/welcome.png";
 import sheikh from "../img/sheikh1.png";
+import jsonData from "../data/data.json";
 
 // import { fainstagram } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -18,54 +19,19 @@ var flag = 0;
 
 function App() {
   const [isTimeOver, setIsTimeOver] = useState(false);
+  const [code, setCode] = useState("");
+  const [showFields, setShowFields] = useState(false);
+  const [couponVal, setCouponVal] = useState("");
+  const [flag, setFlag] = useState(false);
 
   const calculateTimeLeft = () => {
-    // var currentLocation = window.location;
-    // console.log(currentLocation);
-    // if (currentLocation.href === "https://www.kunationalday.ae/"){
-    //   window.location.href = "https://khalifa-university.web.app/";
-    //   // console.log("YEE");
-    // }
+    // const difference = new Date().getHours();
+    var timeLeft = new Date().getMinutes();
+    // console.log(jsonData.coupon1);
 
-    const difference = new Date(2020, 10, 24, 11) - new Date();
-    // const difference = new Date(2020, 10, 23, 12, 12) - new Date();
-    let timeLeft = {};
+    timeLeft = timeLeft % 10;
+    // console.log(timeLeft);
 
-    // console.log(window.innerWidth, window.innerHeight);
-    // console.log("difference", difference);
-
-    if (difference > 0) {
-      var dd = Math.floor(difference / (1000 * 60 * 60 * 24));
-      var hh = Math.floor((difference / (1000 * 60 * 60)) % 24);
-      var mm = Math.floor((difference / 1000 / 60) % 60);
-      var ss = Math.floor((difference / 1000) % 60);
-      if (dd > 0) {
-        hh = hh + 24;
-      } else if (hh < 10) {
-        hh = "0" + hh;
-      }
-      if (mm < 10) {
-        mm = "0" + mm;
-      }
-      if (ss < 10) {
-        ss = "0" + ss;
-      }
-      timeLeft = {
-        // days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: hh,
-        minutes: mm,
-        seconds: ss,
-      };
-    }
-    if (difference < 1 && flag === 0) {
-      // if (ss < 3 && flag === 0) {
-      // setIsTimeOver(true);
-      // setTimeout(() => {
-      // window.location.href = "https://holofair.app/thh/?event=zone&THH";
-      // }, 10000);
-      // flag = 1;
-    }
-    // console.log("timeLeft: ", timeLeft.seconds);
     return timeLeft;
   };
 
@@ -75,131 +41,345 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setTimeLeft(calculateTimeLeft());
+
+      if (timeLeft === 0) {
+        setCouponVal(jsonData.coupon0);
+      } else if (timeLeft === 1) {
+        setCouponVal(jsonData.coupon1);
+      } else if (timeLeft === 2) {
+        setCouponVal(jsonData.coupon2);
+      } else if (timeLeft === 3) {
+        setCouponVal(jsonData.coupon3);
+      } else if (timeLeft === 4) {
+        setCouponVal(jsonData.coupon4);
+      } else if (timeLeft === 5) {
+        setCouponVal(jsonData.coupon5);
+      } else if (timeLeft === 6) {
+        setCouponVal(jsonData.coupon6);
+      } else if (timeLeft === 7) {
+        setCouponVal(jsonData.coupon7);
+      } else if (timeLeft === 8) {
+        setCouponVal(jsonData.coupon8);
+      } else if (timeLeft === 9) {
+        setCouponVal(jsonData.coupon9);
+      }
     }, 1000);
   });
+  const handleSubmitCode = (e) => {
+    // var d = new Date();
+    // console.log("date: ", d);
 
-  const timerComponents = [];
+    e.preventDefault();
+    // setLoader(true);
 
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
+    if (couponVal === code) {
+      console.log("Access Granted!");
+      setShowFields(true);
     }
+  };
 
-    timerComponents.push(<span>{timeLeft[interval]} </span>);
-  });
+  const [hear, setHear] = useState("");
+  const [collab, setCollab] = useState("");
+  const [rate, setRate] = useState("");
+  const [handle, setHandle] = useState("");
+  const [campaignrun, setCampaignrun] = useState("");
+  const [prodquality, setProdquality] = useState("");
+  const [flexible, setFlexible] = useState("");
+  const [satisfied, setSatisfied] = useState("");
+  const [message1, setMessage1] = useState("");
+  const [message2, setMessage2] = useState("");
+
+  const [loader, setLoader] = useState(false);
+
+  const [state, setState] = useState({ x: 25});
+
+  const handleSubmit = (e) => {
+    var d = new Date();
+    console.log("date: ", d);
+
+    e.preventDefault();
+    setLoader(true);
+
+
+    // db.collection("clientsurvey")
+    //   .add({
+    //     'Where did you hear about us?': hear,
+    //     'How long have you been collaborating with THH?': collab,
+    //     "How would you rate the activation?": rate,
+    //     "How well Account manager handle the project?": handle,
+    //     "Did the campaign run smoothly?": campaignrun,
+    //     "Rate the production Quality": prodquality,
+    //     "How flexible was THH during the project?": flexible,
+    //     "How satisfied are you with THH's service?": satisfied,
+    //     "Suggestions and Feedback": message1,
+    //     "Testimonial": message2,
+    //     dateTime: String(d),
+    //   })
+    //   .then(() => {
+    //     alert("Message has been submitted!");
+    //     setLoader(false);
+    //   })
+    //   .catch((error) => {
+    //     alert(error.message);
+    //     setLoader(false);
+    //   });
+
+    setHear("");
+    setCollab("");
+    setRate("");
+    setHandle("");
+    setCampaignrun("");
+    setProdquality("");
+    setFlexible("");
+    setSatisfied("");
+    setMessage1("");
+    setMessage2("");
+  }
+
+  console.log(code);
   return (
     <>
-      <div
-        className="instruction-page"
-        style={{ visibility: isTimeOver ? "hidden" : "visible" }}
-      >
-        <div className="section-1">
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <img
-                  alt="Khalifa University"
-                  className="img-fluid float-left tLeft"
-                  src={topLeft}
-                ></img>
-              </div>
-              <div className="col">
-                <img
-                  alt="Seeds Of The Union"
-                  className="img-fluid float-right tright"
-                  src={topRight}
-                ></img>
-              </div>
-            </div>
-          </div>
+      <div className="outer-div">
+        <div className="text-center main-text">
+          <h1>Your Coupon Is: {timeLeft}</h1>
+          <h1>Your Coupon Is: {couponVal}</h1>
         </div>
-        <div className="container mt-3 mainContainer">
-          <div className="row">
-            <div className="col-sm-4">
-              <div className="row">
-                <div className="col-12">
-                  <div className="sidebar-content">
-                    <img
-                      alt="Welcome to KU Virtual National Day Ceremony 2020"
-                      className="img-fluid float-left"
-                      src={welcome}
-                    ></img>
-                    <br />
-                    {/* <h3> Tuesday, 24 November 2020</h3> */}
-                    <h3>Coming Soon!</h3>
-                    <br />
-                  </div>
-                </div>
-                <div className="col-12">
-                  <div className="sidebar-content">
-                    <div className="timer">
-                      <h2>
-                        {/* <div className="timerText">
-                          <div className="row text-center numbers">
-                            <div className="col-4 col-xs-4">
-                              {timerComponents[0]}
-                            </div>
-                            <div className="col-4 col-xs-4">
-                              {timerComponents[1]}
-                            </div>
-                            <div className="col-4 col-xs-4">
-                              {timerComponents[2]}
-                            </div>
-                          </div>
-                          <div className="row text-center texts">
-                            <div className="col-4 col-xs-4">
-                              <span>Hours</span>
-                            </div>
-                            <div className="col-4 col-xs-4">
-                              <span>Minutes</span>
-                            </div>
-                            <div className="col-4 col-xs-4">
-                              <span>Seconds</span>
-                            </div>
-                          </div>
-                        </div> */}
-                      </h2>
-                      {/* <div className="soon-text">
-                        <h1>Coming Soon!</h1>
-                      </div> */}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-8">
-              <div className="main-content">
-                <img
-                  alt="Sheikh"
-                  className="img-fluid float-right sheikh"
-                  src={sheikh}
-                ></img>
-              </div>
-            </div>
-          </div>
-        </div>
+        {/* <div className="input-fields text-center">
+          <h2>Enter the code:</h2>
+          <input
+            required
+            type="text"
+            value={code}
+            id="Company Website"
+            onChange={(e) => setCode(e.target.value)}
+            name="hear"
+          />
+        <button>Submit</button>
+        </div> */}
 
-        <div className="timeSection">
-          <h3>
-            #KU2020NationalDay {"\u00A0"}
-            {"\u00A0"}
-            {"\u00A0"}
-            {"\u00A0"}
-            {/* <div className="social-icons"> */}
-            <a href="https://www.instagram.com/khalifa_university/">
-              <FontAwesomeIcon icon={faInstagram} style={{ color: "black" }} />{" "}
-            </a>
-            <a href="https://www.facebook.com/khalifauniversity/">
-              <FontAwesomeIcon icon={faFacebook} style={{ color: "black" }} />{" "}
-            </a>
-            <a href="https://www.youtube.com/user/KhalifaUniversity">
-              <FontAwesomeIcon icon={faYoutube} style={{ color: "black" }} />{" "}
-            </a>
-            <a href="https://twitter.com/KhalifaUni">
-              <FontAwesomeIcon icon={faTwitter} style={{ color: "black" }} />{" "}
-            </a>
-            {/* </div> */}
-          </h3>
+        <div className="input-fields text-center">
+          <form className="form" onSubmit={handleSubmitCode}>
+            <h2>Enter the code:</h2>
+
+            <input
+              required
+              type="text"
+              value={code}
+              id="Coupon Code"
+              onChange={(e) => setCode(e.target.value)}
+              name="Coupon Code"
+            />
+            <button type="submit">Submit</button>
+
+            <div style={{visibility: showFields? "visible" : "hidden"}}>
+            {/* <div style={{ visibility: showFields ? "hidden" : "visible" }}> */}
+              <form className="form" onSubmit={handleSubmit}>
+                {/* <img src={Logo} alt="logo" /> */}
+                <h1>
+                  your testimonial<span>.</span>
+                </h1>
+
+                <label>
+                  1. How did you find out about The Hanging House/ Where did you
+                  first hear about us?
+                </label>
+                <div className="radio-inputs">
+                  <input
+                    required
+                    type="radio"
+                    value="Company Website"
+                    id="Company Website"
+                    onChange={() => setHear("Company Website")}
+                    name="hear"
+                  />
+                  <label for="male">Company Website</label>
+
+                  <input
+                    type="radio"
+                    value="Google ads/search"
+                    id="Google ads/search"
+                    onChange={() => setHear("Google ads/search")}
+                    name="hear"
+                  />
+                  <label for="Google ads/search">Google ads/search</label>
+
+                  <input
+                    type="radio"
+                    value="Social Media"
+                    id="Social Media"
+                    onChange={() => setHear("Social Media")}
+                    name="hear"
+                  />
+                  <label for="Social Media">Social Media</label>
+
+                  <input
+                    type="radio"
+                    value="Word of mouth"
+                    id="Word of mouth"
+                    onChange={() => setHear("Word of mouth")}
+                    name="hear"
+                  />
+                  <label for="Word of mouth">Word of mouth</label>
+
+                  <input
+                    type="radio"
+                    value="Emails"
+                    id="Emails"
+                    onChange={() => setHear("Emails")}
+                    name="hear"
+                  />
+                  <label for="Emails">Emails</label>
+                </div>
+
+                <label>
+                  2. How long have you been collaborating with The Hanging
+                  House?
+                </label>
+                <select
+                  required
+                  value={collab}
+                  onChange={(e) => setCollab(e.target.value)}
+                >
+                  <option value="">Please Select...</option>
+                  <option value="1-3 months">1-3 months</option>
+                  <option value="4-6 month">4-6 months</option>
+                  <option value="7-12 months">7-12 months</option>
+                  <option value="Over a year">Over a year</option>
+                </select>
+
+
+                <select
+                  required
+                  value={rate}
+                  onChange={(e) => setRate(e.target.value)}
+                >
+                  <option value="">Please Select...</option>
+                  <option value="Satisfied">Satisfied</option>
+                  <option value="Very satisfied">Very satisfied</option>
+                  <option value="Dissatisfied">Dissatisfied</option>
+                  <option value="Very dissatisfied">Very dissatisfied</option>
+                </select>
+
+                <label>
+                  4. How well did your Account Manager handle your project?
+                </label>
+                <select
+                  required
+                  value={handle}
+                  onChange={(e) => setHandle(e.target.value)}
+                >
+                  <option value="">Please Select...</option>
+                  <option value="Well">Well</option>
+                  <option value="Extremely well">Extremely well</option>
+                  <option value="Not so well">Not so well</option>
+                  <option value="Not at all well ">Not at all well</option>
+                </select>
+
+                <label>
+                  5. From an Operations point of view, did the production
+                  technical aspects of the campaign run smoothly?{" "}
+                </label>
+                <select
+                  required
+                  value={campaignrun}
+                  onChange={(e) => setCampaignrun(e.target.value)}
+                >
+                  <option value="">Please Select...</option>
+                  <option value="Yes, it went perfectly">
+                    Yes, it went perfectly
+                  </option>
+                  <option value="Yes, but there were some problems">
+                    Yes, but there were some problems
+                  </option>
+                  <option value="No, it didn’t go smoothly but there were Improvements">
+                    No, it didn’t go smoothly but there were Improvements
+                  </option>
+                  <option value="No, it went horribly altogether">
+                    No, it went horribly altogether
+                  </option>
+                </select>
+
+                <label>
+                  6. How would you rate the production quality of your campaign?{" "}
+                </label>
+                <select
+                  required
+                  value={prodquality}
+                  onChange={(e) => setProdquality(e.target.value)}
+                >
+                  <option value="">Please Select...</option>
+                  <option value="High quality ">High quality </option>
+                  <option value="Very high quality ">Very high quality </option>
+                  <option value="Low quality">Low quality</option>
+                  <option value="Very low quality">Very low quality</option>
+                </select>
+
+                <label>
+                  7. How flexible was the The Hanging House team throughout the
+                  project?{" "}
+                </label>
+                <select
+                  required
+                  value={flexible}
+                  onChange={(e) => setFlexible(e.target.value)}
+                >
+                  <option value="">Please Select...</option>
+                  <option value="Flexible">Flexible</option>
+                  <option value="Very flexible">Very flexible</option>
+                  <option value="Somewhat flexible">Somewhat flexible</option>
+                  <option value="None at all">None at all</option>
+                </select>
+
+                <label>
+                  8. Overall, how satisfied were you with The Hanging House ’s
+                  services?{" "}
+                </label>
+                <select
+                  required
+                  value={satisfied}
+                  onChange={(e) => setSatisfied(e.target.value)}
+                >
+                  <option value="">Please Select...</option>
+                  <option value="Satisfied">Satisfied</option>
+                  <option value="Very satisfied">Very satisfied</option>
+                  <option value="Neutral">Neutral</option>
+                  <option value="Not so satisfied">Not so satisfied</option>
+                  <option value="Not satisfied at all">
+                    Not satisfied at all
+                  </option>
+                </select>
+
+                <label>
+                  9. Do you have any suggestions and feedback you would like to
+                  share? Were there any concerns that you feel were overlooked?
+                </label>
+                <textarea
+                  placeholder=""
+                  value={message1}
+                  onChange={(e) => setMessage1(e.target.value)}
+                ></textarea>
+
+                <label>
+                  10. Kindly write a few words as a testimonial below
+                </label>
+                <textarea
+                  placeholder=""
+                  value={message2}
+                  onChange={(e) => setMessage2(e.target.value)}
+                ></textarea>
+
+                <button
+                  type="submit"
+                  style={{
+                    background: loader ? "#acc" : "rgb(147, 213, 0)",
+                    color: "rgb(53, 52, 54)",
+                  }}
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </form>
         </div>
       </div>
     </>
